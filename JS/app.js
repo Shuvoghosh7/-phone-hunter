ff32e670047a7c28e64b2e61f720c97d978e2d22
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-03-01 02:14:55 +0600

    fetch data and shoe data in  Ui

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,33 @@
-alert('ccc')
+const getPhones = () =>{
+    const searchValue = document.getElementById("search-box")
+    const searchText = searchValue.value
+    const url =`https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showPhones(data.data))
+
+    searchValue.value=''
+}
+
+
+const showPhones = (phones) => {
+    console.log(phones)
+    const phoneContainer = document.getElementById("phone-container")
+    phoneContainer.textContent=''
+    phones.forEach(phone => {
+        const div = document.createElement("div")
+        div.classList.add('col')
+        div.innerHTML = `
+        <div class="card">
+        <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${phone.phone_name}</h5>
+          <p>${phone.brand}</p>
+        </div>
+      </div>
+        `
+    phoneContainer.appendChild(div)
+        
+    });
+    
+} 
