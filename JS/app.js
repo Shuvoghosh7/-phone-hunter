bd564be1a0c9a7a561ae6b70ad6d8fbf17e6fdea
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-03-01 03:27:51 +0600

    fetch phone details ans set phone details in UI

--- JS/app.js
+++ JS/app.js
@@ -1,17 +1,19 @@
 const getPhones = () =>{
     const searchValue = document.getElementById("search-box")
     const searchText = searchValue.value
+    if(searchText == ''){
+        document.getElementById('error').style.display='block'
+    }else{
+    document.getElementById('error').style.display='none'
     const url =`https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
     .then(res => res.json())
-    .then(data => showPhones(data.data))
-
+    .then(data =>showPhones(data.data))
     searchValue.value=''
 }
-
-
+}
+// Show data in webside
 const showPhones = (phones) => {
-    console.log(phones)
     const phoneContainer = document.getElementById("phone-container")
     phoneContainer.textContent=''
     phones.forEach(phone => {
@@ -19,10 +21,11 @@ const showPhones = (phones) => {
         div.classList.add('col')
         div.innerHTML = `
         <div class="card">
-        <img src="${phone.image}" class="card-img-top" alt="...">
+        <img  src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${phone.phone_name}</h5>
           <p>${phone.brand}</p>
+          <button onclick="phoneDetails('${phone.slug}')" class="btn btn-secondary text-white" >Details</button>
         </div>
       </div>
         `
@@ -31,3 +34,25 @@ const showPhones = (phones) => {
     });
     
 } 
+// phone details
+const phoneDetails = (id) =>{
+    const url =`https://openapi.programming-hero.com/api/phone/${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setPhoneDetails(data.data))
+}
+const setPhoneDetails = (info) =>{
+    
+   const phoneDetails= document.getElementById("details-container")
+   phoneDetails.innerHTML=`
+   <img src="${info.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+    <p>${info.releaseDate}</p>
+        
+      
+    </div>
+   `
+    console.log(info)
+}
+
