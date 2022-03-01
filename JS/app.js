796104b9735f09a7888e6a0ebffe7621791a99f0
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-03-01 14:01:08 +0600

    add some commit and fix

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,7 @@
 const getPhones = () =>{
     const searchValue = document.getElementById("search-box")
     const searchText = searchValue.value
-    if(searchText == ''){
+    if(searchText === ''){
         document.getElementById('error').style.display='block'
     }
     else{
@@ -16,13 +16,17 @@ const getPhones = () =>{
 // Show phones data in webside
 const showPhones = (phones) => {  
   const phoneContainer = document.getElementById("phone-container")
+  if(phones.length === 0){
+    document.getElementById('error2').style.display='block'
+  }else{
+    document.getElementById('error2').style.display='none'
     phoneContainer.textContent=''
     phones.slice(0,20).forEach(phone => {
         const div = document.createElement("div")
         div.classList.add('col')
         div.innerHTML = `
-        <div class="card">
-        <img  src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card p-3 border rounded bg-light">
+        <img  src="${phone.image}" class="card-img-top mx-auto" alt="...">
         <div class="card-body">
           <h5 class="card-title">${phone.phone_name}</h5>
           <p>${phone.brand}</p>
@@ -33,6 +37,7 @@ const showPhones = (phones) => {
     phoneContainer.appendChild(div)
         
     });
+  }
     
 } 
 // phone details 
@@ -55,7 +60,7 @@ const setPhoneDetails = (info) =>{
     <p><b>Display Size:</b> ${info.mainFeatures.displaySize}</p>  
     <p><b>Memory:</b> ${info.mainFeatures.memory}</p>  
     <p><b>storage:</b> ${info.mainFeatures.storage}</p>  
-    <p><b>sensors:</b> ${info.mainFeatures.sensors[0]},${info.mainFeatures.sensors[1]},${info.mainFeatures.sensors[2]},${info.mainFeatures.sensors[3]},${info.mainFeatures.sensors[4]},${info.mainFeatures.sensors[5]},${info.mainFeatures.sensors[6]}</p>  
+    <p><b>sensors:</b><br> ${info.mainFeatures.sensors[0]},  ${info.mainFeatures.sensors[1]},${info.mainFeatures.sensors[2]},${info.mainFeatures.sensors[3]},<br>${info.mainFeatures.sensors[4]},${info.mainFeatures.sensors[5]},${info.mainFeatures.sensors[6]}</p>  
     
     <b class="text-success">Other Information</b>
     <p><b>Bluetooth:</b>${info?.others?.Bluetooth}</p> 
