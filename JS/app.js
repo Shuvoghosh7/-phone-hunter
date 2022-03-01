dbc812abea19d9a5938dbac5327daaa62a6aecc6
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-03-01 18:52:20 +0600

    fix some problem and add comment

--- JS/app.js
+++ JS/app.js
@@ -4,16 +4,16 @@ const getPhones = () =>{
     //get spanner by ID
     document.getElementById('spanner').style.display='block'
     if(searchText === ''){
-        document.getElementById('error').style.display='block'
-        document.getElementById('error2').style.display='none'
+        document.getElementById('errorFor-empty').style.display='block'
+        document.getElementById('errorFor-string').style.display='none'
     }
     else{
-    document.getElementById('error').style.display='none'
+    document.getElementById('errorFor-empty').style.display='none'
     const url =`https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
     .then(res => res.json())
     .then(data => {
-      if(data.data==null){
+      if(data.data === null){
         document.getElementById('spanner').style.display='block'
       }else{
         document.getElementById('spanner').style.display='none'
@@ -23,14 +23,14 @@ const getPhones = () =>{
     searchValue.value=''
 }
 }
-// Show phones data in webside
+// Show phones data in UI
 const showPhones = (phones) => {  
   const phoneContainer = document.getElementById("phone-container")
   if(phones.length === 0){
-    document.getElementById('error2').style.display='block'
+    document.getElementById('errorFor-string').style.display='block'
     document.getElementById('spanner').style.display='block'
   }else{
-    document.getElementById('error2').style.display='none'
+    document.getElementById('errorFor-string').style.display='none'
     phoneContainer.textContent=''
     phones.slice(0,20).forEach(phone => {
         const div = document.createElement("div")
@@ -62,15 +62,18 @@ const phoneDetails = (id) =>{
 const setPhoneDetails = (info) =>{
    const phoneDetails= document.getElementById("details-container")
    phoneDetails.innerHTML=`
-   <img class="w-50" src="${info.image}" class="card-img-top" alt="...">
+   <img src="${info.image}" class="card-img-top w-50" alt="...">
     <div class="card-body">
     <h5 class="card-title">${info.name}</h5>
     <p><b>ReleaseDate:</b> ${info.releaseDate == ''? 'Not released yet. Coming very soon' : info.releaseDate }</p>
+
+    <b class="text-success">Main Features</b>
     <p><b>Chipset:</b> ${info.mainFeatures.chipSet}</p>  
     <p><b>Display Size:</b> ${info.mainFeatures.displaySize}</p>  
     <p><b>Memory:</b> ${info.mainFeatures.memory}</p>  
     <p><b>storage:</b> ${info.mainFeatures.storage}</p>  
-    <p><b>sensors:</b><br> ${info.mainFeatures.sensors[0]},  ${info.mainFeatures.sensors[1]},${info.mainFeatures.sensors[2]},${info.mainFeatures.sensors[3]},<br>${info.mainFeatures.sensors[4]},${info.mainFeatures.sensors[5]},${info.mainFeatures.sensors[6]}</p>  
+    
+    <p><b class="text-success">sensors:</b><br> ${info.mainFeatures.sensors[0]},  ${info.mainFeatures.sensors[1]},${info.mainFeatures.sensors[2]},${info.mainFeatures.sensors[3]},<br>${info.mainFeatures.sensors[4]},${info.mainFeatures.sensors[5]},${info.mainFeatures.sensors[6]}</p>  
     
     <b class="text-success">Other Information</b>
     <p><b>Bluetooth:</b>${info?.others?.Bluetooth}</p> 
@@ -86,6 +89,5 @@ const setPhoneDetails = (info) =>{
     <p><b>WLAN:</b> ${info.others?.WLAN}</p> 
     </div>
    `
-    console.log(info)
 }
 
